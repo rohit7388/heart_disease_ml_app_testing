fcc1fdf963129319a86c39cdd09193944a5e1b9e
Ramith.s <86591506+rohit7388@users.noreply.github.com>
2025-07-25 11:19:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,10 +12,13 @@ def home():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        data = [float(request.form[i]) for i in request.form]
-        data = np.array(data).reshape(1, -1)
-        prediction = model.predict(data)[0]
-        result = 'Heart Disease Detected' if prediction == 1 else 'No Heart Disease'
+        try:
+            data = [float(request.form[i]) for i in request.form]
+            data = np.array(data).reshape(1, -1)
+            prediction = model.predict(data)[0]
+            result = 'Heart Disease Detected' if prediction == 1 else 'No Heart Disease'
+        except:
+            result = 'Invalid input. Please enter valid numbers.'
         return render_template('index.html', prediction=result)
 
 if __name__ == '__main__':
